4433bcc4920dfeef1ff3e6267156a12b61de23c1
TheChosenIndividual <120053489+TheChosenIndividual@users.noreply.github.com>
2024-02-16 18:54:50 -0500

    Just marking things

--- src/main/java/com/hbm/blocks/machine/HeaterHeatex.java
+++ src/main/java/com/hbm/blocks/machine/HeaterHeatex.java
@@ -1,3 +1,6 @@
+
+//Importing functions like HBM packages
+
 package com.hbm.blocks.machine;
 
 import java.util.ArrayList;
@@ -19,20 +22,21 @@ import net.minecraft.world.World;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.Pre;
 import net.minecraftforge.common.util.ForgeDirection;
 
+//Creates the class that makes the block function
 public class HeaterHeatex extends BlockDummyable implements ILookOverlay, ITooltipProvider {
-
+        //sets what level of tool the player needs to break it efficiently
 	public HeaterHeatex() {
 		super(Material.iron);
 	}
-
+        //creates the tile entity
 	@Override
 	public TileEntity createNewTileEntity(World world, int meta) {
-		
+		//checks for fluids in the heater
 		if(meta >= 12) return new TileEntityHeaterHeatex();
 		if(hasExtra(meta)) return new TileEntityProxyCombo().fluid();
 		return null;
 	}
-
+        //Not sure what this means, but im pretty sure its related to hitboxes and the actual size of the model
 	@Override
 	public int[] getDimensions() {
 		return new int[] {0, 0, 1, 1, 1, 1};
@@ -42,12 +46,12 @@ public class HeaterHeatex extends BlockDummyable implements ILookOverlay, IToolt
 	public int getOffset() {
 		return 1;
 	}
-	
+	//Detects wether the block is active
 	@Override
 	public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int side, float hitX, float hitY, float hitZ) {
 		return this.standardOpenBehavior(world, x, y, z, player, 0);
 	}
-
+        //Pretty sure this is related to making machines like the boiler absorb the created heat (hence why you put it on top)
 	@Override
 	public void printHook(Pre event, World world, int x, int y, int z) {
 		
@@ -62,12 +66,12 @@ public class HeaterHeatex extends BlockDummyable implements ILookOverlay, IToolt
 			return;
 		
 		TileEntityHeaterHeatex heater = (TileEntityHeaterHeatex) te;
-
+                //Sets the little pop up that appears when you hover over the heater with your cursor
 		List<String> text = new ArrayList();
 		text.add(String.format(Locale.US, "%,d", heater.heatEnergy) + " TU");
 		ILookOverlay.printGeneric(event, I18nUtil.resolveKey(getUnlocalizedName() + ".name"), 0xffff00, 0x404000, text);
 	}
-
+        //sets the input ports for the heater in the world, then he just mixes this with the actual model texture
 	@Override
 	public void fillSpace(World world, int x, int y, int z, ForgeDirection dir, int o) {
 		super.fillSpace(world, x, y, z, dir, o);
@@ -80,7 +84,7 @@ public class HeaterHeatex extends BlockDummyable implements ILookOverlay, IToolt
 		this.makeExtra(world, x - 1, y, z + 1);
 		this.makeExtra(world, x - 1, y, z - 1);
 	}
-
+        //Adds information to...somewhere
 	@Override
 	public void addInformation(ItemStack stack, EntityPlayer player, List list, boolean ext) {
 		this.addStandardInfo(stack, player, list, ext);
